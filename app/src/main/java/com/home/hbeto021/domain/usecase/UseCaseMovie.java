54be23410c478f657a45955f30f190683066bb54
Hbeto021 <humberto.cesar01@outlook.com>
2019-04-23 09:40:27 -0300

    Create methods in UseCaseMovie that interact with repository

--- app/src/main/java/com/home/hbeto021/domain/usecase/UseCaseMovie.java
+++ app/src/main/java/com/home/hbeto021/domain/usecase/UseCaseMovie.java
@@ -1,6 +1,34 @@
 package com.home.hbeto021.domain.usecase;
 
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+
+import com.home.hbeto021.data.database.repository.MovieRepository;
+import com.home.hbeto021.domain.model.Movie;
+
+import java.util.List;
+
 public class UseCaseMovie {
+
+    private MovieRepository repository;
+
+    public UseCaseMovie(Application application) {
+        repository = new MovieRepository(application);
+    }
+
+    public boolean insert(Movie movie) {
+        if (movie != null && movie.movieIsValid()) {
+            repository.insertMovie(movie);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public LiveData<List<Movie>> getAllMovies() {
+        return repository.selectAllMovies();
+    }
 
 
 }
