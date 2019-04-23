479061ca08157c2e1ecfbddc07f37d9702bd0159
Hbeto021 <humberto.cesar01@outlook.com>
2019-04-23 17:22:14 -0300

    Create the method that send a message to user

--- app/src/main/java/com/home/hbeto021/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/home/hbeto021/viewmodel/MainActivityViewModel.java
@@ -4,18 +4,17 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.media.midi.MidiOutputPort;
 import android.support.annotation.NonNull;
 
 import com.home.hbeto021.domain.model.Movie;
 import com.home.hbeto021.domain.usecase.UseCaseMovie;
 
-import java.nio.channels.MulticastChannel;
 import java.util.List;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
     private UseCaseMovie ucMovie;
+    private MutableLiveData<String> msgToUser = new MutableLiveData<>();
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
@@ -23,11 +22,20 @@ public class MainActivityViewModel extends AndroidViewModel {
 
     }
 
-    public boolean insertMovie(Movie movie) {
-        return ucMovie.insert(movie);
+    public void insertMovie(String titleMovie, String genreMovie) {
+        if (ucMovie.insert(titleMovie, genreMovie)) {
+            msgToUser.setValue("Movie has been added.");
+        } else {
+            msgToUser.setValue("Fail, there are some empty fields.");
+        }
+    }
+
+    public MutableLiveData<String> getMessageToUser() {
+        return msgToUser;
     }
 
     public LiveData<List<Movie>> getAllMovies() {
         return ucMovie.getAllMovies();
     }
+
 }
